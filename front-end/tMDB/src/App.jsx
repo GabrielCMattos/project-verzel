a2f023f16490ae4978e545a53fd6d708e2782887
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-10-25 20:12:21 -0300

    Remove código não utilizado e adiciona componente Navbar ao projeto

--- front-end/tMDB/src/App.jsx
+++ front-end/tMDB/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Link, Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p className="read-the-docs">
-        tMDB
-      </p>
-    </>
+    <div className="App">
+      
+      <h2>Movies Lib</h2>
+      <Outlet />
+    </div>
   )
 }
 
---
+++ front-end/tMDB/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+<nav id="nav">
+    <h2>
+        <Link to="/">Verzel Movies</Link>
+    </h2>
+</nav>
