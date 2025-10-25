f8260bbb107f3cea74f3c105a241819121ed7e38
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-10-25 20:20:26 -0300

    Refatora Navbar para melhorar a estrutura do código e adiciona estilos básicos ao projeto

--- front-end/tMDB/src/components/Navbar.jsx
+++ front-end/tMDB/src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
-import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 const Navbar = () => {
     return (
         <nav id="nav">
             <h2>
-                <Link to="/">Verzel Movies</Link>
+                <Link to="/">
+                    <BiCameraMovie />Verzel Movies
+                </Link>
             </h2>
             <form>
                 <input type="text" placeholder="Busque um filme" />
-                <button type="submit"></button>
+                <button type="submit">
+                    <BiSearchAlt2 />
+                </button>
             </form>
         </nav>
     )
