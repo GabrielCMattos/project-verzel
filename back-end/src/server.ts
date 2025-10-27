e056ad95f0214b87b8adc540935f6badd1388c06
Gabriel Mattos <gabriel.casamattos1011@gmail.com>
2025-10-26 22:28:48 -0300

    Fazendo config no perfil do usuário

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routers/userRouter";
+import movieRouter from "./routers/movieRouter";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+app.use("/api/movies", movieRouter);
 app.use("/api/users", userRouter);
 
 app.listen(process.env.PORT || 3301, () =>
